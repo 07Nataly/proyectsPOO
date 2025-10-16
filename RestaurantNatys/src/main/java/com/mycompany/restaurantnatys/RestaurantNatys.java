b9ed8ee4a53c2e4de7ee85c609dfddcf3c4d90e9
07Nataly <nmesa@unipacifico.edu.co>
2025-10-15 23:09:32 -0500

    RestaurantNatys

---
+++ RestaurantNatys/src/main/java/com/mycompany/restaurantnatys/RestaurantNatys.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantnatys;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class RestaurantNatys {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
